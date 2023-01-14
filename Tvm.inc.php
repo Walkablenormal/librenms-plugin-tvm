7c4b0c7887f5e6e311764ab7cfc890177abcbbfa
Walkablenormal <rubenvankomen@gmail.com>
2023-01-14 11:40:45 +0100

    Addition of filter.

--- Tvm.inc.php
+++ Tvm.inc.php
@@ -1,6 +1,8 @@
 <?php
 
-echo "<html><body><table>\n\n";
+echo "<input type='text' id='tvm_filter' onkeyup='filterFunction()' placeholder='Search for devices..'>";
+
+echo "<html><body><table id='tvm_table'>\n\n";
 $f = fopen("/opt/librenms/html/plugins/Data/file.csv", "r");
 while (($line = fgetcsv($f)) !== false) {
         echo "<tr>";
@@ -13,3 +15,27 @@ fclose($f);
 echo "\n</table></body></html>";
 
 ?>
+
+<script>
+function filterFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("tvm_filter");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tvm_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
