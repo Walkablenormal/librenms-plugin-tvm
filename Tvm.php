2c04b71de7e6ef66f1e7b9e9d8c35b95e705da3c
Walkablenormal <rubenvankomen@gmail.com>
2023-01-14 12:47:28 +0100

    Test if the plugin still works like this.

--- Tvm.php
+++ Tvm.php
@@ -13,15 +13,9 @@ class Tvm
 
     public function device_overview_container($device)
     {
-        echo '<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>' . get_class() . ' Plugin </strong> </div>';
-        echo '  Example plugin in "Device - Overview" tab <br>';
-        echo '</div></div></div></div>';
     }
 
     public function port_container($device, $port)
     {
-        echo '<div class="container-fluid"><div class="row"> <div class="col-md-12"> <div class="panel panel-default panel-condensed"> <div class="panel-heading"><strong>' . get_class() . ' plugin in "Port" tab</strong> </div>';
-        echo 'Example display in Port tab</br>';
-        echo '</div></div></div></div>';
     }
 }
