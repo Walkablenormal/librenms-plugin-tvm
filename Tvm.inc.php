cbae822423ffee89af11f92594674bdf0584e0b6
Walkablenormal <rubenvankomen@gmail.com>
2023-01-14 11:34:48 +0100

    Dynamic table generation.

--- Tvm.inc.php
+++ Tvm.inc.php
@@ -1,5 +1,15 @@
 <?php
 
-echo 'Well done, the plugin system is up and running';
+echo "<html><body><table>\n\n";
+$f = fopen("/opt/librenms/html/plugins/Data/file.csv", "r");
+while (($line = fgetcsv($f)) !== false) {
+        echo "<tr>";
+        foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+        }
+        echo "</tr>\n";
+}
+fclose($f);
+echo "\n</table></body></html>";
 
 ?>
