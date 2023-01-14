a1233b3844e1305c42072f50c2623427486b0b29
Walkablenormal <rubenvankomen@gmail.com>
2023-01-14 12:32:49 +0100

    Capitalization of the CSS-folder name.

--- Tvm.inc.php
+++ Tvm.inc.php
@@ -29,7 +29,7 @@ echo "\n</table></body></html>";
 }
 
 #tvm_filter {
-  background-image: url('/css/magnifyingGlass.png');
+  background-image: url('/CSS/magnifyingGlass.png');
   background-position: 10px 10px;
   background-repeat: no-repeat;
   width: 100%;
